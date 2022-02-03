192d6d036ca5412dc2e0f57eda4a20b71bc7040b
Austin Cronin <austin.cronin3d@gmail.com>
2022-02-03 08:01:37 -0500

    restarted build of project files for clean

--- player.js
+++
@@ -1,8 +0,0 @@
-const Player = (name) => {
-    const marker = name;
-    // choose a space on the board
-    const move = (space) => {
-        return space;
-    }
-    return { marker, move };
-}
