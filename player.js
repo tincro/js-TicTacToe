87d7826ec89cca1e629d5583c8e75b176d9204e8
Austin Cronin <austin.cronin3d@gmail.com>
2022-02-03 08:57:57 -0500

    added first pass of player object

---
+++ player.js
@@ -0,0 +1,8 @@
+const player = (name) => {
+    const marker = name;
+    const move = (space) => {
+        // change space value to marker value
+        console.log(space);
+    }
+    return {move};
+}
